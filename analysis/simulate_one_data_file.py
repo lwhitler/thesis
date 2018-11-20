73a92e99f16b38542cc2f625c1f7a274e7508570
Lily Whitler <whitler.lily@gmail.com>
2018-11-19 23:19:15 -0700

    Add progress print statements, remove unnecessary enumerate

--- analysis/simulate_one_data_file.py
+++ analysis/simulate_one_data_file.py
@@ -12,6 +12,7 @@ filename = 'zen.2458106.' + JD_dec + '.xx.HH.uv'
 data_in = '/data6/HERA/data/IDR2.1/2458106/' + filename
 sim_out = '/home/lwhitler/data/dfiles/sim/' + filename.replace('.uv', '.sim.uv')
 
+print('Loading data...')
 # Load the real data to start from
 uvd = UVData()
 uvd.read(data_in, file_type='miriad')
@@ -24,6 +25,7 @@ bls = np.unique(uvd.baseline_array)
 pols = uvutils.polnum2str(uvd.polarization_array)
 ants = list(set(uvd.ant_1_array).union(set(uvd.ant_2_array)))
 
+print('Finding unique baselnes...')
 # Redundant baseline groups in the real data
 unique_bls, bl_inds = np.unique(uvd.baseline_array, return_index=True)
 reds, _, lens, conj_bls = uvutils.get_baseline_redundancies(unique_bls, uvd.uvw_array[bl_inds],
@@ -40,20 +42,24 @@ Tsky_model = {pol: noise.HERA_Tsky_mdl[pol] for pol in pols}
 Tsky = {pol: noise.resample_Tsky(freqs, lsts, Tsky_mdl=Tsky_model[pol]) for pol in pols}
 Trx = 150.
 
+print('Simulating antenna gains...')
 # Antenna gains
 gains = sigchain.gen_gains(freqs, ants)
 
+print('Simulating RFI...')
 # RFI
 rfi_narrow = rfi.rfi_stations(freqs, lsts)
 rfi_broad = rfi.rfi_impulse(freqs, lsts, chance=0.01)
 rfi_scatter = rfi.rfi_scatter(freqs, lsts, chance=0.001)
 rfi_all = rfi_narrow + rfi_broad + rfi_scatter
 
+print('Simulating foregrounds...')
 # Combine foregrounds, noise, RFI, and antenna gains
 bl_dict = {bl: np.zeros((len(lsts), len(freqs), len(pols))) for bl in bls}
-for i, (red_group, bl_len) in enumerate(zip(reds, lens_ns)):
+for red_group, bl_len in enumerate(zip(reds, lens_ns)):
     pt_src = foregrounds.pntsrc_foreground(lsts, freqs, bl_len, nsrcs=200)
-    for j, pol in enumerate(pols):
+    for k, pol in enumerate(pols):
+        print('Starting polarization {0}'.format(pol))
         diff = foregrounds.diffuse_foreground(Tsky_model[pol], lsts, freqs, bl_len)
         true_vis = diff + pt_src
         for bl in red_group:
@@ -70,7 +76,7 @@ for i, (red_group, bl_len) in enumerate(zip(reds, lens_ns)):
             # Apply antenna gains and save
             bl_tuple = uvd.baseline_to_antnums(bl)
             g_ij = gains[bl_tuple[0]] * gains[bl_tuple[1]].conj()
-            bl_dict[bl][:, :, j] = xtalk_noisy_vis * g_ij
+            bl_dict[bl][:, :, k] = xtalk_noisy_vis * g_ij
 
 # Set any missed baselines to NaNs (should just be the autos)
 print('Setting the following baselines to NaNs:')
@@ -85,6 +91,7 @@ for bl in bl_dict.keys():
     blt_ind = np.where(uvd.baseline_array == bl)[0]
     sim_data[blt_ind] = bl_dict[bl][:, None, :, :]
 
+print('Saving...')
 # Save to a miriad file
 # Unflag everything just in case anything was flagged
 uvd_sim.flag_array = np.full_like(uvd.flag_array, False)
